f9aaa272f23f8b2fcbe97f231384f84ab5884454
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-11 22:46:40 +0100

    Remove non existing favicon and add description

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,7 @@ const Head = () => (
   <>
     <title>sdorra.dev</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="sdorra.dev" />
-    <link rel="icon" href="/favicon.svg" />
+    <meta name="description" content="This are some of Sebastian Sdorra's notes polished for a blog" />
   </>
 );
 

6c35991cc746bd13baecb504a13a125bb137e29a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-15 19:39:17 +0100

    Change format of inline code

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -14,7 +14,9 @@ const Markdown = ({ code }: Props) => {
     <section
       className={clsx(
         "prose prose-zinc dark:prose-invert",
-        "hover:prose-a:decoration-cyan-500 hover:prose-a:decoration-2"
+        "hover:prose-a:decoration-cyan-500 hover:prose-a:decoration-2",
+        // colors are taken from the shiki theme, which is configure in the contentlayer.config.js
+        "prose-code:rounded prose-code:bg-[rgb(46,52,64)] prose-code:px-1 prose-code:font-normal prose-code:text-[rgb(216,222,233)] prose-code:before:content-none prose-code:after:content-none dark:prose-code:text-[rgb(143,188,187)]"
       )}
     >
       <MDXComponent components={{ Notification }} />

adaad3a4d1230e061aa252cff7c1a47d54f8625a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-16 20:48:21 +0100

    Improve inline code layout

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -14,6 +14,9 @@ const Markdown = ({ code }: Props) => {
     <section
       className={clsx(
         "prose prose-zinc dark:prose-invert",
+        "prose-code:px-0.5 prose-code:font-normal prose-code:border prose-code:rounded prose-code:before:content-none prose-code:after:content-none",
+        "prose-code:border-zinc-300 prose-code:bg-zinc-100 prose-code:text-zinc-700",
+        "dark:prose-code:border-zinc-500 dark:prose-code:bg-zinc-700 dark:prose-code:text-zinc-100",
         "hover:prose-a:decoration-cyan-500 hover:prose-a:decoration-2"
       )}
     >

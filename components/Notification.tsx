a31b5c1f111101e35baafc5c18240df12a3ee0c4
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-24 20:10:11 +0100

    Add blog post about edge function size limit

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,11 +1,18 @@
+import clsx from "clsx";
 import { ReactNode } from "react";
 
 type Props = {
+  severity?: "warning" | "error";
   children: ReactNode;
 };
 
-const Notification = ({ children }: Props) => (
-  <div className="not-prose rounded-md border-2 border-amber-800 bg-amber-300 p-5 text-amber-900 shadow-md [&>*]:text-amber-900">
+const Notification = ({ children, severity = "warning" }: Props) => (
+  <div
+    className={clsx("not-prose rounded-md border-2 p-5 shadow-md", {
+      "border-amber-800 bg-amber-300 text-amber-900 [&>*]:text-amber-900": severity === "warning",
+      "border-red-800 bg-red-300 text-red-900 [&>*]:text-red-900": severity === "error",
+    })}
+  >
     {children}
   </div>
 );

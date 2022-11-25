5848c445c97ec51dd5a453eb2d9a9fb80c72e6b4
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-25 22:20:11 +0100

    Improve post card layout on smallest view port

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Post } from "contentlayer/generated";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,21 +12,28 @@ const PostCard = ({ post }: Props) => (
   <article>
     <Link
       href={`/posts/${post._raw.flattenedPath}`}
-      className="group mt-10 flex flex-col gap-2 rounded-md border-2 border-zinc-300 p-4 hover:border-cyan-500 hover:bg-zinc-100 dark:border-zinc-700 dark:hover:border-cyan-400 dark:hover:bg-zinc-700"
+      className={clsx(
+        "group mt-10 flex flex-col gap-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-700",
+        "border-2 border-zinc-300 p-4 hover:border-cyan-500 dark:border-zinc-700 dark:hover:border-cyan-400"
+      )}
     >
-      <figure className="flex items-stretch gap-4">
+      <figure className="flex items-stretch justify-center gap-4 sm:justify-start">
         <Image
           src={post.image}
           alt="Featured Image"
           width={128}
           height={128}
-          className="relative -mt-10 h-32 w-32 flex-shrink-0 rounded-md border-2 border-zinc-300 object-cover group-hover:border-cyan-500 dark:border-zinc-700 dark:group-hover:border-cyan-400"
+          className={clsx(
+            "relative -mt-10 mb-2 h-32 w-32 flex-shrink-0 rounded-md bg-white object-cover  dark:bg-zinc-800 sm:mb-0",
+            "border-2 border-zinc-300 group-hover:border-cyan-500 dark:border-zinc-700 dark:group-hover:border-cyan-400"
+          )}
         />
-        <div className="inline-flex flex-col justify-between gap-2">
+        <div className="hidden flex-col justify-between gap-2 sm:inline-flex">
           <h3 className="text-2xl font-semibold">{post.title}</h3>
           <p className="hidden text-lg sm:block">{post.summary}</p>
         </div>
       </figure>
+      <h3 className="text-2xl font-semibold sm:hidden">{post.title}</h3>
       <p className="text-lg sm:hidden">{post.summary}</p>
       <footer className="flex justify-between pt-2 text-xs">
         <p>{post.readingTime}</p>

0f70c0a0561847361f56281a58b8bf8d17003512
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-11-25 21:48:36 +0100

    Add vercel success log to the end of the post

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -2,15 +2,16 @@ import clsx from "clsx";
 import { ReactNode } from "react";
 
 type Props = {
-  severity?: "warning" | "error";
+  type?: "success" | "warning" | "error";
   children: ReactNode;
 };
 
-const Notification = ({ children, severity = "warning" }: Props) => (
+const Notification = ({ children, type = "warning" }: Props) => (
   <div
-    className={clsx("not-prose rounded-md border-2 p-5 shadow-md", {
-      "border-amber-800 bg-amber-300 text-amber-900 [&>*]:text-amber-900": severity === "warning",
-      "border-red-800 bg-red-300 text-red-900 [&>*]:text-red-900": severity === "error",
+    className={clsx("not-prose rounded-md border-2 p-5 shadow-md overflow-x-scroll", {
+      "border-amber-800 bg-amber-300 text-amber-900 [&>*]:text-amber-900": type === "warning",
+      "border-red-800 bg-red-300 text-red-900 [&>*]:text-red-900": type === "error",
+      "border-emerald-800 bg-emerald-300 text-emerald-900 [&>*]:text-emerald-900": type === "success",
     })}
   >
     {children}

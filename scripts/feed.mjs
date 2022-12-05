9bb28fe06e361e73f68549abc23a15dade3fe751
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-12-05 21:15:19 +0100

    Add content to rss feed

--- scripts/feed.mjs
+++ scripts/feed.mjs
@@ -1,7 +1,36 @@
-import { Feed } from "feed";
+import { evaluateSync } from "@mdx-js/mdx";
 import { compareDesc, parseISO } from "date-fns";
-import { allPosts } from "../.contentlayer/generated/index.mjs";
+import { Feed } from "feed";
 import { writeFile } from "fs/promises";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import * as runtime from "react/jsx-runtime";
+import remarkGfm from "remark-gfm";
+import { allPosts } from "../.contentlayer/generated/index.mjs";
+
+const convertContent = (body) => {
+  const mdx = evaluateSync(body, {
+    ...runtime,
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  }).default;
+
+  const CH = ({ children }) => React.createElement("div", null, children);
+  CH.Code = ({ children }) => React.createElement("div", null, children);
+  CH.Code.displayName = "CH.Code";
+
+  const components = {
+    CH,
+    Notification: ({ children }) => React.createElement("div", null, children),
+    FileTree: () => null,
+  };
+
+  return renderToString(
+    React.createElement(mdx, {
+      components,
+    })
+  );
+};
 
 const createFeed = () => {
   const feed = new Feed({
@@ -14,35 +43,38 @@ const createFeed = () => {
     favicon: "https://sdorra.dev/favicon.ico",
     copyright: "All rights reserved 2022, Sebastian Sdorra",
     feedLinks: {
-      atom: "https://sdorra.dev/atom.xml"
+      atom: "https://sdorra.dev/atom.xml",
     },
     author: {
       name: "Sebastian Sdorra",
       email: "s.sdorra@gmail.com",
-      link: "https://sdorra.dev"
-    }
+      link: "https://sdorra.dev",
+    },
   });
 
-  allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date))).forEach(post => {
-    const url = `https://sdorra.dev/posts/${post._raw.flattenedPath}`;
-    feed.addItem({
-      title: post.title,
-      id: url,
-      link: url,
-      description: post.summary,
-      author: [
-        {
-          name: "Sebastian Sdorra",
-          email: "s.sdorra@gmail.com",
-          link: "https://sdorra.dev"
-        }
-      ],
-      date: parseISO(post.date),
-      //image: ""
+  allPosts
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+    .forEach((post) => {
+      const url = `https://sdorra.dev/posts/${post._raw.flattenedPath}`;
+      feed.addItem({
+        title: post.title,
+        id: url,
+        link: url,
+        description: post.summary,
+        author: [
+          {
+            name: "Sebastian Sdorra",
+            email: "s.sdorra@gmail.com",
+            link: "https://sdorra.dev",
+          },
+        ],
+        date: parseISO(post.date),
+        content: convertContent(post.body.raw),
+        //image: ""
+      });
     });
-  });
 
-  return feed.rss2()
+  return feed.rss2();
 };
 
 (async () => {

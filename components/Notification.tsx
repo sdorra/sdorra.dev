3eedd61506560663c0d03121ed1cde020a52cb2b
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2022-12-27 10:44:56 +0100

    A hover effect to notification links

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -20,12 +20,15 @@ const Notification = ({ children, type = "warning" }: Props) => {
   const Icon = icons[type];
   return (
     <div
-      className={clsx("not-prose relative overflow-x-scroll rounded-md border-2 p-5 shadow-md [&_a]:underline", {
-        "border-sky-800 bg-sky-300 text-sky-900 [&>*:not(svg)]:text-sky-900": type === "info",
-        "border-amber-800 bg-amber-300 text-amber-900 [&>*:not(svg)]:text-amber-900": type === "warning",
-        "border-red-800 bg-red-300 text-red-900 [&>*:not(svg)]:text-red-900": type === "error",
-        "border-emerald-800 bg-emerald-300 text-emerald-900 [&>*:not(svg)]:text-emerald-900": type === "success",
-      })}
+      className={clsx(
+        "not-prose relative overflow-x-scroll rounded-md border-2 p-5 shadow-md [&_a]:underline [&_a]:hover:decoration-2",
+        {
+          "border-sky-800 bg-sky-300 text-sky-900 [&>*:not(svg)]:text-sky-900": type === "info",
+          "border-amber-800 bg-amber-300 text-amber-900 [&>*:not(svg)]:text-amber-900": type === "warning",
+          "border-red-800 bg-red-300 text-red-900 [&>*:not(svg)]:text-red-900": type === "error",
+          "border-emerald-800 bg-emerald-300 text-emerald-900 [&>*:not(svg)]:text-emerald-900": type === "success",
+        }
+      )}
     >
       {children}
       <Icon

7504811c3fc79a7c6e9f909bb7755db58cae2910
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-01-06 19:48:24 +0100

    Add canonical url to meta tags

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -9,24 +9,29 @@ type Props = {
 const scheme = process.env.NEXT_PUBLIC_SCHEME ? process.env.NEXT_PUBLIC_SCHEME : "https";
 const fqdn = process.env.NEXT_PUBLIC_FQDN ? process.env.NEXT_PUBLIC_FQDN : "sdorra.dev";
 
-const Meta = ({ title, description, ogType = "website", url = "/", ogImage }: Props) => (
-  <>
-    <title>{title}</title>
-    <meta name="description" content={description} />
+const Meta = ({ title, description, ogType = "website", url = "/", ogImage }: Props) => {
+  const completeUrl = `${scheme}://${fqdn}${url}`;
+  return (
+    <>
+      <title>{title}</title>
+      <link rel="canonical" href={completeUrl} />
 
-    <meta property="og:url" content={`${scheme}://${fqdn}${url}`} />
-    <meta property="og:type" content={ogType} />
-    <meta name="og:title" content={title} />
-    <meta name="og:description" content={description} />
-    {ogImage ? <meta property="og:image" content={`${scheme}://${fqdn}${ogImage}`} /> : null}
+      <meta name="description" content={description} />
 
-    <meta name="twitter:card" content={ogImage ? "summary_large_image" : "summary"} />
-    <meta name="twitter:domain" content={fqdn} />
-    <meta property="twitter:url" content={`${scheme}://${fqdn}${url}`} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    {ogImage ? <meta property="twitter:image" content={`${scheme}://${fqdn}${ogImage}`} /> : null}
-  </>
-);
+      <meta property="og:url" content={completeUrl} />
+      <meta property="og:type" content={ogType} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      {ogImage ? <meta property="og:image" content={`${scheme}://${fqdn}${ogImage}`} /> : null}
+
+      <meta name="twitter:card" content={ogImage ? "summary_large_image" : "summary"} />
+      <meta name="twitter:domain" content={fqdn} />
+      <meta property="twitter:url" content={completeUrl} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {ogImage ? <meta property="twitter:image" content={`${scheme}://${fqdn}${ogImage}`} /> : null}
+    </>
+  );
+};
 
 export default Meta;

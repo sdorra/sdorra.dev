f50393f3e212d71f013ddbba9f0b41f239a3b414
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-01-19 21:28:35 +0100

    Redesign embedded content

--- components/EmbeddedCard.tsx
+++ components/EmbeddedCard.tsx
@@ -1,29 +1,83 @@
 import clsx from "clsx";
 import { LucideProps } from "lucide-react";
 import { ComponentType, ReactNode } from "react";
+import Avatar from "./Avatar";
 
 type Props = {
   href: string;
-  icon: ComponentType<LucideProps>;
   className?: string;
   children?: ReactNode;
 };
 
-const EmbeddedCard = ({ href, className, children, icon: Icon }: Props) => (
+const EmbeddedCard = ({ href, className, children }: Props) => (
   <div className="not-prose">
     <a
       href={href}
       target="_blank"
       rel="noreferrer"
       className={clsx(
-        "group relative mb-2 flex rounded-md border-2 p-5 shadow-md hover:border-cyan-600 dark:border-zinc-700 dark:hover:border-cyan-400",
+        "group mb-2 block space-y-2 rounded-md p-5 shadow-md",
+        "border-l-8 border-r border-t border-b",
+        "border-zinc-300 dark:border-zinc-600",
+        "hover:border-cyan-600 dark:hover:border-cyan-400",
         className
       )}
     >
       {children}
-      <Icon className="absolute top-2 right-2 stroke-2 text-zinc-400 group-hover:stroke-[3px] group-hover:text-cyan-600 dark:text-zinc-500 dark:group-hover:text-cyan-400" />
     </a>
   </div>
 );
+
+type IconProps = {
+  icon: ComponentType<LucideProps>;
+  className?: string;
+};
+
+const EmbeddedIcon = ({ icon: Icon, className }: IconProps) => (
+  <Icon
+    className={clsx(
+      "flex-shrink-0 stroke-2 group-hover:stroke-[3px]",
+      "text-zinc-400 dark:text-zinc-500",
+      "group-hover:text-cyan-600 dark:group-hover:text-cyan-400",
+      className
+    )}
+  />
+);
+
+type TitleProps = {
+  children?: ReactNode;
+  icon: ComponentType<LucideProps>;
+};
+
+const Title = ({ children, icon }: TitleProps) => (
+  <div className="flex justify-between">
+    {children}
+    <EmbeddedIcon icon={icon} />
+  </div>
+);
+
+Title.displayName = "EmbeddedCard.Title";
+
+EmbeddedCard.Title = Title;
+
+type AuthorProps = {
+  name: string;
+  avatar?: {
+    src: string;
+    alt: string;
+  };
+};
+
+const Author = ({ name, avatar }: AuthorProps) => (
+  <div className="flex items-center gap-2">
+    {avatar ? <Avatar src={avatar.src} alt={avatar.alt} size={32} className="h-8 w-8" /> : null}
+
+    <p>{name}</p>
+  </div>
+);
+
+Author.displayName = "EmbeddedCard.Author";
+
+EmbeddedCard.Author = Author;
 
 export default EmbeddedCard;
--- components/StaticTweet.tsx
+++ components/StaticTweet.tsx
@@ -9,18 +9,15 @@ type Props = {
 };
 
 const StaticTweet = ({ tweet }: Props) => (
-  <EmbeddedCard href={tweet.url} icon={Twitter} className="gap-5">
-    <Avatar
-      src={tweet.author.profileImageUrl}
-      alt={`Profile of ${tweet.author.name}`}
-      size={64}
-      className="w-8 md:h-16 h-8 md:w-16"
+  <EmbeddedCard href={tweet.url} className="gap-5">
+    <EmbeddedCard.Title icon={Twitter}>
+      <p className="text-zinc-500 dark:text-zinc-400">Tweet from <DateTime value={tweet.createdAt}/></p>
+    </EmbeddedCard.Title>
+    <blockquote dangerouslySetInnerHTML={{ __html: tweet.text }} />
+    <EmbeddedCard.Author
+      name={tweet.author.name}
+      avatar={{ src: tweet.author.profileImageUrl, alt: `Profile of ${tweet.author.name}` }}
     />
-    <div className="flex flex-col gap-2">
-      <p className="text-lg font-bold">{tweet.author.name}</p>
-      <blockquote dangerouslySetInnerHTML={{ __html: tweet.text }} />
-      <DateTime value={tweet.createdAt} className="text-xs text-zinc-500 dark:text-zinc-400" />
-    </div>
   </EmbeddedCard>
 );
 

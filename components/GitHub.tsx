bcae292b3d11a7ee89edca17601db0f811c5f2dc
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-01-19 21:28:35 +0100

    Redesign embedded content

--- components/GitHub.tsx
+++ components/GitHub.tsx
@@ -17,20 +17,22 @@ type Props =
     };
 
 const EmbeddedEntity = ({ repository, entity, type }: Props) => (
-  <EmbeddedCard href={entity.url} icon={Github} className="flex-col gap-2">
-    <p className="text-lg font-bold">
-      <span className="text-zinc-500 dark:text-zinc-400">
-        {repository.owner}/{repository.name}
-      </span>{" "}
-      <span dangerouslySetInnerHTML={{ __html: entity.titleHTML }} />
-    </p>
+  <EmbeddedCard href={entity.url}>
+    <EmbeddedCard.Title icon={Github}>
+      <p className="text-lg font-bold">
+        <span className="text-zinc-500 dark:text-zinc-400">
+          {repository.owner}/{repository.name}
+        </span>{" "}
+        <span dangerouslySetInnerHTML={{ __html: entity.titleHTML }} />
+      </p>
+    </EmbeddedCard.Title>
     <p className="text-sm">
       {type} #{entity.number} opened on <DateTime value={entity.createdAt} /> by
     </p>
-    <div className="flex items-center gap-2">
-      <Avatar src={entity.author.avatarUrl} alt={`Profile of ${entity.author.login}`} size={32} className="h-8 w-8" />
-      <p>{entity.author.login}</p>
-    </div>
+    <EmbeddedCard.Author
+      name={entity.author.login}
+      avatar={{ src: entity.author.avatarUrl, alt: `Profile of ${entity.author.login}` }}
+    />
   </EmbeddedCard>
 );
 

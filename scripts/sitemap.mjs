adde8d249074131c85bf8616caa7ea8b9989e778
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-02 14:23:41 +0100

    Add priorities to sitemap entries

--- scripts/sitemap.mjs
+++ scripts/sitemap.mjs
@@ -16,12 +16,22 @@ const mapPathToEntries = (p) => {
       if (p.lastModification) {
         url.lastmod = p.lastModification;
       }
+      url.priority = "0.64";
       return url;
     });
+  } else if (p === "/") {
+    return [
+      {
+        loc: createUrl(p),
+        lastmod: new Date().toISOString(),
+        priority: "1.00",
+      },
+    ];
   }
   return [
     {
       loc: createUrl(p),
+      priority: "0.80",
     },
   ];
 };
@@ -45,7 +55,9 @@ const createEntries = async () => {
 const createSitemap = async (routes) => {
   const sitemap = `
   <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${routes
         .map((route) => {
           let url = "<url>\n";

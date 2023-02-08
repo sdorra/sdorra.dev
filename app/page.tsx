eb4ba56a43863aaea270fb2c41d4085d05227787
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-08 14:11:56 +0100

    Add more details to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
 import PostCard from "components/PostCard";
 import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
+import Image from "next/image";
 
 const Home = () => (
   <>
-    <h1 className="mb-4 text-6xl font-bold">
-      sdorra<span className="text-cyan-600 dark:text-cyan-400">.dev</span>
-    </h1>
-    <p>This are some of my notes polished for a blog</p>
+    <div className="mb-5 sm:mb-10">
+      <div className="mb-2 flex items-center gap-4">
+        <Image
+          className="h-16 w-16 flex-shrink-0 rounded-full border-2 border-zinc-400 dark:border-zinc-600 shadow-md sm:h-24 sm:w-24"
+          width="96"
+          height="96"
+          src="https://avatars.githubusercontent.com/u/493333"
+          alt="Avatar of Sebastian"
+        />
+        <div>
+          <h1 className="mb-2 text-5xl font-bold sm:text-6xl">
+            sdorra<span className="text-cyan-600 dark:text-cyan-400">.dev</span>
+          </h1>
+          <p className="hidden text-xl sm:block">A site about development by Sebastian Sdorra</p>
+        </div>
+      </div>
+      <p className="text-xl sm:hidden">A site about development by Sebastian Sdorra</p>
+    </div>
+
+    <p className="text-xl">
+      Welcome to my site. On this page I will share my experiences and learnings on my way as a developer. Most of the
+      posts will be about web development, but who knows what I&apos;ll be dealing with next.
+    </p>
+
     <h2 className="mt-10 mb-4 text-4xl font-semibold">Posts</h2>
     <section className="space-y-4">
       {allPosts

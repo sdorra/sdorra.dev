3d902d83c4e47e8d5b38015f9e7343bab6a18495
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-10 19:46:35 +0100

    Improve PostCard layout

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -23,11 +23,11 @@ const PostCard = ({ post }: Props) => (
           blurDataURL={post.imageBlurDataURL}
           placeholder={post.imageBlurDataURL ? "blur" : "empty"}
           alt="Featured Image"
-          width={128}
+          width={256}
           height={128}
           priority={true}
           className={clsx(
-            "relative -mt-10 mb-2 h-32 w-32 flex-shrink-0 rounded-md bg-white object-cover  dark:bg-zinc-800 sm:mb-0",
+            "relative -mt-10 mb-2 h-32 w-64 flex-shrink-0 rounded-md bg-white object-cover dark:bg-zinc-800 sm:mb-0 sm:w-32",
             "border-2 border-zinc-300 group-hover:border-cyan-500 dark:border-zinc-700 dark:group-hover:border-cyan-400"
           )}
         />

bfe4f3cb2b995d30c6493d4b5005bf74e739fa0a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-10 14:21:52 +0100

    Remove duplicate content by using grid instead of flex

--- components/SiteHeading.tsx
+++ components/SiteHeading.tsx
@@ -7,23 +7,18 @@ type Props = {
 const SiteHeading = ({ isTitle = false }: Props) => {
   const Heading = isTitle ? "h1" : "span";
   return (
-    <div className="mb-5 sm:mb-10">
-      <div className="mb-2 flex items-center gap-4">
-        <Image
-          className="h-16 w-16 flex-shrink-0 rounded-full border-2 border-zinc-400 shadow-md dark:border-zinc-600 sm:h-24 sm:w-24"
-          width="96"
-          height="96"
-          src="https://avatars.githubusercontent.com/u/493333"
-          alt="Avatar of Sebastian"
-        />
-        <div>
-          <Heading className="mb-2 text-5xl font-bold sm:text-6xl">
-            sdorra<span className="text-cyan-600 dark:text-cyan-400">.dev</span>
-          </Heading>
-          <p className="hidden text-xl sm:block">A site about development by Sebastian Sdorra</p>
-        </div>
-      </div>
-      <p className="text-xl sm:hidden">A site about development by Sebastian Sdorra</p>
+    <div className="mb-5 grid grid-cols-[4.5rem,1fr] items-center sm:mb-10 sm:grid-cols-[7rem,1fr]">
+      <Image
+        className="row-span-2 h-16 w-16 rounded-full border-2 border-zinc-400 shadow-md dark:border-zinc-600 sm:h-24 sm:w-24"
+        width="96"
+        height="96"
+        src="https://avatars.githubusercontent.com/u/493333"
+        alt="Avatar of Sebastian"
+      />
+      <Heading className="mb-2 text-5xl font-bold sm:text-6xl">
+        sdorra<span className="text-cyan-600 dark:text-cyan-400">.dev</span>
+      </Heading>
+      <p className="col-span-2 mt-2 text-xl sm:col-span-1 sm:mt-0">A site about development by Sebastian Sdorra</p>
     </div>
   );
 };

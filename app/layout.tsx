43ce160613b1af3b0375b5127ee004893b8f8cd8
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-10 14:04:20 +0100

    Add favicons and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,11 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => (
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="alternate" type="application/rss+xml" title="sdorra.dev rss feed" href="/rss.xml" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <script
         dangerouslySetInnerHTML={{
           __html: `

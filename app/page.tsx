c3507a5bb8f80abfd24317ee0b5fe2741c339288
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-13 19:45:35 +0100

    Add new header to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import PostCard from "components/PostCard";
+import SiteHeading from "components/SiteHeading";
 import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 
 const Home = () => (
   <>
-    <h1 className="mb-4 text-6xl font-bold">
-      sdorra<span className="text-cyan-600 dark:text-cyan-400">.dev</span>
-    </h1>
-    <p>This are some of my notes polished for a blog</p>
+    <SiteHeading />
     <h2 className="mt-10 mb-4 text-4xl font-semibold">Posts</h2>
     <section className="space-y-4">
       {allPosts

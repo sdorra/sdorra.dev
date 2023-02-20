5e3b535241df9639b92969c97d6cb48d4f575c5f
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-20 14:20:10 +0100

    Fix missing title on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { compareDesc } from "date-fns";
 
 const Home = () => (
   <>
-    <SiteHeading />
+    <SiteHeading isTitle />
     <h2 className="mt-10 mb-4 text-4xl font-semibold">Posts</h2>
     <section className="space-y-4">
       {allPosts

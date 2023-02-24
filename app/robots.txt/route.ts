8589bffd27474da5b506dad9470a554125a62c29
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-02-24 13:22:41 +0100

    Allow bots only on production deployment

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -1,6 +1,8 @@
 import { baseUrl } from "lib/config";
 
 const content = `User-Agent: *
+${process.env.VERCEL_ENV === "production" ? "Allow: /" : "Disallow: /"}
+
 Sitemap: ${baseUrl}/sitemap.xml
 `;
 

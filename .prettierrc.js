88270bfe89c381377a8a3cb5fc11f3240b3cb005
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-12-20 14:49:44 +0100

    Update prettier and plugins

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,24 +1,8 @@
-/* @see https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31#issuecomment-1024722576 */
-const tailwindcss = require("prettier-plugin-tailwindcss");
-const organizeImports = require("prettier-plugin-organize-imports");
-
-const merged = {
-  ...tailwindcss,
-  parsers: {
-    ...tailwindcss.parsers,
-    ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
-      acc[key] = {
-        ...tailwindcss.parsers[key],
-        preprocess(code, options) {
-          return organizeImports.parsers[key].preprocess(code, options);
-        },
-      };
-      return acc;
-    }, {}),
-  },
-};
-
-module.exports = {
-  plugins: [merged],
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
   printWidth: 120,
 };
+
+
+module.exports = config;
